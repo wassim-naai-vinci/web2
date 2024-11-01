e78fa0ca30669d0e2ba34219de3f6b09463f6162
wassim-naai-vinci <wassim.naai@student.vinci.be>
2024-11-01 17:55:10 +0100

    new: ex1.3

--- exercices/1.3/routes/films.ts
+++ exercices/1.3/routes/films.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 
-import { Film } from "../types";
+import { Film, NewFilm } from "../types";
 
 const router = Router();
 
@@ -62,8 +62,83 @@ const defaultFilms: Film[] = [
 ];
 // Read all films
 
-router.get("/", (_req, res) => {
-  return res.json(defaultFilms);
+router.get("/", (req, res) => {
+
+  if (!req.query["minimum-duration"]) {
+    return res.json(defaultFilms);    
+  }
+  const minDuration = Number(req.query["minimum-duration"]);
+
+  if (isNaN(minDuration) || minDuration <= 0) {
+    res.json("Wrong minimum Duration");
+  }
+
+  const filteredFilms = defaultFilms.filter((film) => film.duration >= minDuration);
+
+  return res.json(filteredFilms);
 });
+
+router.get("/:id", (req, res) => {
+
+  const id = Number(req.params.id);
+
+  // verifier toujours si l'id est un nombre :
+  if (isNaN(id)) {
+    return res.json("Wrong minimum id");
+  }
+
+  const films = defaultFilms.find((film) => film.id === id);
+
+  // verifier si l'id du film existe
+  if (!films) {
+    return res.json("Film not found -> wrong id");
+  }
+
+  return res.json(films);
+});
+
+router.post("/", (req, res) => {
+  const body: unknown = req.body;
+  
+  if (
+    !body ||
+    typeof body !== "object" ||
+    !("title" in body) ||
+    !("director" in body) ||
+    !("duration" in body) ||
+    typeof body.title !== "string" ||
+    typeof body.director !== "string" ||
+    typeof body.duration !== "number" ||
+    !body.title.trim() ||
+    !body.director.trim() ||
+    body.duration <= 0 ||
+    ("budget" in body && (typeof body.budget !== "number" || body.budget <= 0)) ||
+    ("description" in body && (typeof body.description !== "string" || !body.description.trim())) ||
+    ("imageUrl" in body && (typeof body.imageUrl !== "string" || !body.imageUrl.trim()))
+  ) {
+    return res.json("Wrong body information/format")
+  }
+
+  const newFilm = body as NewFilm;
+
+  /* 
+      REDUCE est utilisé pour trouver l'id maximum.
+      - reduce prend une fonction qui s'applique à chaque élément de la liste (drink) en utilisant un accumulateur (maxId).
+      - À chaque itération, cette fonction compare l'id de la boisson actuelle (drink.id) avec la valeur de l'accumulateur maxId.
+      - Si l'id de la boisson actuelle est plus grand que maxId, alors maxId prend cette valeur (drink.id). Sinon, maxId conserve sa valeur.
+      - La valeur initiale de maxId est définie à 0 dans reduce((maxId, drink) => ..., 0). Cela permet à maxId de commencer à 0, et si drinks est vide, maxId reste 0
+      - Une fois que reduce a trouvé le plus grand id dans drinks, le code ajoute 1 pour créer le nextId, c'est-à-dire l'id du nouvel élément qui sera ajouté.
+  */
+  const nextId = defaultFilms.reduce((maxId,defaultFilms) => (defaultFilms.id > maxId ? defaultFilms.id : maxId),0) +1;
+
+  //Fusion de Propriétés : crée un nouvel objet qui combine l'id généré (nextId) avec toutes les autres propriétés de newFilm
+  const addFilm : Film = {id : nextId, ...newFilm};
+
+  defaultFilms.push(addFilm);
+  
+
+  return res.json(addFilm);
+});
+
 
 export default router;
